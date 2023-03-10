a610796f3e916e4318f50a9f94a62d37968ba0f8
ps-yu <pankajsharma12540@gmail.com>
2023-03-09 22:01:17 -0500

    Add story to check the appointments view

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -192,3 +192,22 @@ storiesOf("Appointment", module)
     onSave={action("onSave")}
     />
   ))
+
+  .add("Empty Story", ()=>(
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer:"LHL" }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  
