16c33313f97eb7ccd8f27bab927c170603508361
ps-yu <pankajsharma12540@gmail.com>
2023-03-09 22:07:06 -0500

    Pass the required form to the InterviewList component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,29 +2,38 @@ import React, {useState} from "react";
 import InterviewerList from "components/InterviewerList"
 import Button from "components/Button";
 
-const [student, setStudent] = useState(props.student || "");
-const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
-
 export default function Form(props){
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function(){
+    setInterviewer(null);
+    setStudent("")
+  }
+  const cancel = function(){
+    reset();
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
         <input
           className="appointment__create-input text--semi-bold"
           name="name"
           type="text"
           placeholder="Enter Student Name"
+          value={student} onChange={(event)=>setStudent(event.target.value)}
         />
       </form>
       <InterviewerList
         interviewers={props.interviewers}
+        onChange={setInterviewer}
       />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger >Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm >Save</Button>
         </section>
       </section>
