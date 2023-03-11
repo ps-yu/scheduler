0512f4a4d1e2b881c3d2b57b45667695c7e3fe4f
ps-yu <pankajsharma12540@gmail.com>
2023-03-11 18:43:43 -0500

    Implement show/hide component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Empty from "./Empty";
+import Header from "./Header";
+import Show from "./Show";
+
 import "./styles.scss";
+
 
 export default function Appointment (props){
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show names={props.interview}/> : <Empty />}
+    </article>
   )
 }
