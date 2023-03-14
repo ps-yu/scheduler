e1b310b3f6a7559add994aca3b98f159d323528d
ps-yu <pankajsharma12540@gmail.com>
2023-03-14 18:41:35 -0400

    Refactor component to SHOW/HIDE FORM

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,15 +2,35 @@ import React from "react";
 import Empty from "./Empty";
 import Header from "./Header";
 import Show from "./Show";
-
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment (props){
+  const { mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show names={props.interview}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+      />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel = {() => back()}
+        />
+      )}
     </article>
   )
 }
