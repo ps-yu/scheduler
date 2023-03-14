d0331f84f6babcdccd95071329a56d2da1cef536
ps-yu <pankajsharma12540@gmail.com>
2023-03-14 18:44:29 -0400

    Helper function to manage the transitions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial){
+  
+  
+  const [mode, setMode] =useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(change, replace=false){
+    if (replace){
+      setHistory([...history.slice(0, history.length - 1), change])
+      setMode(change)
+      return;
+    }
+    setMode(change)
+    setHistory([...history,change])
+  }
+  function back(){
+    if (history.length > 1){
+      const arr = [...history];
+      arr.pop();
+      setHistory([...arr]);
+      setMode(arr[arr.length-1]);
+    }
+  }
+  return {mode,transition, back };
+}
