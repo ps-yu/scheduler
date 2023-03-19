727012bf54d5435bd8970a57e63813bb219e8189
ps-yu <pankajsharma12540@gmail.com>
2023-03-19 17:45:30 -0400

    Add funciton to create interviews

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import {getAppointmentsForDay,getInterview}  from "../helpers/selectors";
+import {getAppointmentsForDay,getInterview, getInterviewersForDay}  from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -16,16 +16,38 @@ export default function Application(props) {
   const setDays = day => setState({ ...state, day });
   
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
+
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-  
+    
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };    
+    return axios.put(`api/appointments/${id}`, appointment)
+      .then((res) => {
+        setState({
+          ...state,
+          appointments
+        })
+        })
+  }
+    
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
       />
     );
   });
