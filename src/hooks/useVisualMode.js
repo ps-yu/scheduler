ea12ce9464eb4f3305d14d76bc36287ab707da5a
ps-yu <pankajsharma12540@gmail.com>
2023-03-23 20:05:15 -0400

    Custom hook for the back, forward states

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial){
-  
-  
   const [mode, setMode] =useState(initial);
   const [history, setHistory] = useState([initial]);
   function transition(change, replace=false){
