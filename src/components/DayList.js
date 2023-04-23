586036d94e7cf52d4b1131cd7508077f72bcde3f
ps-yu <pankajsharma12540@gmail.com>
2023-04-23 15:11:03 -0400

    Fix the props to show the selected day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,12 +3,14 @@ import DayListItem from './DayListItem';
 
 export default function DayList(props){
   const days = props.days.map((day) => {
+    console.log("Day.name",day.name)
+    console.log("props", props)
     return (
         <DayListItem 
           key = {day.id}
           name = {day.name}
           spots = {day.spots}
-          selected = {day.name === props.value}
+          selected = {day.name === props.day}
           setDay = {props.onChange}
         />
     )
